d29d086939a226b291108dbd4d70b16c15e320d9
st-ngabui <nga.bui@supremetech.vn>
2021-05-11 09:51:48 +0700

    convert to js from ts

---
+++ excercise-e-shop/ts/index.ts
@@ -0,0 +1,53 @@
+export interface IProduct {
+  id: number;
+  name: string;
+  image: string;
+  price: number;
+  discount: number;
+}
+
+export type IProductCart = IProduct & {
+  quantity: number;
+};
+export function fetchData(): IProduct[] {
+  return [
+    {
+      id: 1,
+      image: "./assets/img/product-1.png",
+      name: "T-Shirt Summer Vibes",
+      price: 89.99,
+      discount: 30,
+    },
+    {
+      id: 2,
+      image: "./assets/img/product-2.png",
+      name: "Loose Knit 3/4 Sleeve",
+      price: 119.99,
+      discount: 0,
+    },
+    {
+      id: 3,
+      image: "./assets/img/product-3.png",
+      name: "Basic Slim Fit T-Shirt",
+      price: 79.99,
+      discount: 0,
+    },
+    {
+      id: 4,
+      image: "./assets/img/product-4.png",
+      name: "Loose Textured T-Shirt",
+      price: 219.99,
+      discount: 0,
+    },
+  ];
+}
+//get cart from localstorage
+export function getCart(): IProductCart[] {
+  return localStorage.getItem("cart") ? JSON.parse(localStorage.getItem("cart")) : [];
+}
+//get quantity product in cart
+export function getQuantity(cart: IProductCart[]): number {
+  return cart.reduce((quantity, product) => {
+    return quantity + +product.quantity;
+  }, 0);
+}
