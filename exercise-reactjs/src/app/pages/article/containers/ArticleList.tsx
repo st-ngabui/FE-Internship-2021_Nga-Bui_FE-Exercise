4dec2f395d532e7601141fb55af0cf8611e89c47
st-ngabui <nga.bui@supremetech.vn>
2021-05-27 09:27:32 +0700

    add interface and throw error

--- exercise-reactjs/src/app/pages/article/containers/ArticleList.tsx
+++ exercise-reactjs/src/app/pages/article/containers/ArticleList.tsx
@@ -1,15 +1,25 @@
 import React from 'react';
 import axios from 'axios';
 
-function ArticleList() {
-  const [articleList, setArticleList] = React.useState([]);
+interface IArticle {
+  title: string;
+  desc: string;
+  author: string;
+  createdAt: string;
+  minsRead: string;
+  category: string;
+  image: string;
+}
+
+const ArticleList = () => {
+  const [articleList, setArticleList] = React.useState<IArticle[]>([]);
   React.useEffect(() => {
-    axios.get('https://6088e20da6f4a300174271e7.mockapi.io/articles')
+    axios.get(`${process.env.API_BASE_URL}/articles`)
       .then(response => {
-        setArticleList(response.data)
+        setArticleList(response.data);
       })
       .catch(error => {
-        console.log(error.response)
+        throw new Error(error);
       });
   }, [])
   return (
