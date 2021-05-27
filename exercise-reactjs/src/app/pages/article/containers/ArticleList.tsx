fa7bb534906bfc975f1875a529bce75615c7aa6d
st-ngabui <nga.bui@supremetech.vn>
2021-05-27 13:58:04 +0700

    fetch data from api

--- exercise-reactjs/src/app/pages/article/containers/ArticleList.tsx
+++ exercise-reactjs/src/app/pages/article/containers/ArticleList.tsx
@@ -1,9 +1,29 @@
 import React from 'react';
+import axios from 'axios';
 
-function ArticleList() {
+interface IArticle {
+  title: string;
+  desc: string;
+  author: string;
+  createdAt: string;
+  minsRead: string;
+  category: string;
+  image: string;
+}
+const ArticleList = () => {
+  const [articleList, setArticleList] = React.useState<IArticle[]>([]);
+  React.useEffect(() => {
+    axios.get(`https://6088e20da6f4a300174271e7.mockapi.io/articles`)
+      .then(response => {
+        setArticleList(response.data);
+      })
+      .catch(error => {
+        console.log(error)
+      });
+  }, [])
   return (
     <div>
-      
+      <p>Article List</p>
     </div>
   );
 }
