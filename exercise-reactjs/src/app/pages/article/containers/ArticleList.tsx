b1ce68d5b3554002040c9b01276de2dbdf6e5319
st-ngabui <nga.bui@supremetech.vn>
2021-05-31 21:26:33 +0700

    use reactHOC to render component

--- exercise-reactjs/src/app/pages/article/containers/ArticleList.tsx
+++ exercise-reactjs/src/app/pages/article/containers/ArticleList.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import Article from './Article';
+import PageRenderer from './PageRenderer';
 
 export interface IArticle {
   id: number;
@@ -12,12 +13,34 @@ export interface IArticle {
   category: string;
   image: string;
 }
+
+interface IPropsArticleList {
+  data: IArticle[];
+}
+
+const ArticleListUI = (props: IPropsArticleList) => {
+  const { data } = props;
+  return (
+    <div className="articles-wrap">
+      <ul className="container article-list">
+        {data.map((article: IArticle) => {
+          return (
+            <li className="article-item" key={article.id}>
+              <Article article={article} />
+            </li>
+          )
+        })}
+      </ul>
+    </div>
+  )
+}
+
+const ArticleListRender = PageRenderer(ArticleListUI);
+
 const ArticleList = () => {
-  const [articleList, setArticleList] = React.useState<IArticle[]>([]);
-  const [isLoading, setLoading] = React.useState(false);
+  const [articleList, setArticleList] = React.useState<IArticle[]>();
 
   React.useEffect(() => {
-    setLoading(true);
     axios.get(`${process.env.REACT_APP_BASE_URL}/articles`)
       .then(response => {
         setArticleList(response.data);
@@ -25,31 +48,9 @@ const ArticleList = () => {
       .catch(error => {
         console.log(error)
       })
-      .finally(() => {
-        setLoading(false);
-      })
-      ;
   }, [])
   return (
-    <div className="articles-wrap">
-      {!isLoading
-        ?
-        (
-          <ul className="container article-list">
-            {articleList.map((article: IArticle) => {
-              return (
-                <li className="article-item" key={article.id}>
-                  <Article article={article} />
-                </li>
-              )
-            })}
-          </ul>
-        )
-        :
-        (
-          <p>Loading ....</p>
-        )}
-    </div>
+    <ArticleListRender data={articleList} />
   );
 }
 
